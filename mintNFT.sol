dbedd0866ddeec6ce3095603308bf5f8f3a6719b
Abdelnasser Hajihashemabad <info.hashemabadi@gmail.com>
2023-04-09 00:17:22 +0300

    Create mintNFT.sol

---
+++ mintNFT.sol
@@ -0,0 +1,12 @@
+myNFTContract.mintNFT(
+    "0x1234567890123456789012345678901234567890", // recipient
+    "https://example.com/metadata/image.json", // metadata URI
+    "Book Name", // name
+    "Category Name", // category
+    "Subject Name", // subject
+    "Author Name", // author
+    "2022-01-01", // publication date
+    "Country Name", // country
+    "Continent Name", // continent
+    "Museum Name" // museum
+);
